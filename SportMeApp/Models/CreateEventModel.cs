5b1671ef194d2a220f698b413f7d536d99352257
annie <huangtc@bu.edu>
2023-11-24 10:01:09 -0500

    merge

---
+++ SportMeApp/Models/CreateEventModel.cs
@@ -0,0 +1,28 @@
+﻿
+using NuGet.Protocol.Core.Types;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportMeApp.Models
+{
+    public class CreateEvent
+    {
+        [Key]
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public double Fee { get; set; }
+
+        // Foreign key 
+        [ForeignKey("Locations")]
+        public int LocationId { get; set; }
+      
+
+        [ForeignKey("Sport")]
+        public int SportId { get; set; }
+   
+
+    }
+}
