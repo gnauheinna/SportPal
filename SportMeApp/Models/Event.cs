594c322111b6af0af5b6e2e346b0109b440c545e
annie <huangtc@bu.edu>
2023-11-24 00:14:25 -0500

    created eventController to add event to database

--- SportMeApp/Models/Event.cs
+++ SportMeApp/Models/Event.cs
@@ -16,14 +16,14 @@ namespace SportMeApp.Models
 
         // Foreign key 
         public int LocationId { get; set; }
-        public Location Location { get; set; }
+        //public Location Location { get; set; }
 
         public int SportId { get; set; }
-        public Sport Sport { get; set; }
+        //public Sport Sport { get; set; }
 
         // indicating many-to-many relationship between events and users
-        public List<UserEvent> UserEvents { get; set; }
-        public List<Message> Messages { get; set; }
+        //public List<UserEvent> UserEvents { get; set; }
+       // public List<Message> Messages { get; set; }
 
     }
 }
