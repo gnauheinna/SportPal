25059947be22f0a34a47dc3127bca1f6937c953f
annie <huangtc@bu.edu>
2023-12-08 18:02:46 -0500

    commented homecontroller

--- SportMeApp/Controllers/HomeController.cs
+++ SportMeApp/Controllers/HomeController.cs
@@ -23,11 +23,12 @@ namespace SportMeApp.Controllers
         {
             return View();
         }
-
+        // funtion to redirect to Google Oauth EZ as 12
         public ActionResult OauthRedirect()
         {
+            // 1. client id of our google cloud project 
             var client_id = "378858678415-the1gfbovl66l9jbmobcufom12a5kche.apps.googleusercontent.com";
-
+            // 2. redirect url for Oauth
             var redirectUrl = "https://accounts.google.com/o/oauth2/v2/auth?" +
                 "scope=https://www.googleapis.com/auth/calendar+https://www.googleapis.com/auth/calendar.events+https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile" +
                 "&access_type=offline&" +
@@ -36,7 +37,7 @@ namespace SportMeApp.Controllers
                 "state=there&" +
                 "redirect_uri=https://localhost:7203/oauth/callback&" +
                 "client_id=" + client_id;
-
+            
             return Redirect(redirectUrl);
         }
         public IActionResult SignOut()
