4cef26796a8857e5b3523221f7afe8e1c1c01cc1
annie <huangtc@bu.edu>
2023-12-08 18:02:16 -0500

    commented event controller

--- SportMeApp/Controllers/EventCreation/EventController.cs
+++ SportMeApp/Controllers/EventCreation/EventController.cs
@@ -21,14 +21,15 @@ namespace SportMeApp.Controllers.EventCreation
             _logger = logger;
         }
         
-
+        // function to create event in database EZ as 123
         [HttpPost ("CreateEvent")]
         public async Task<IActionResult> CreateEvent(CreateEvent eventData)
         {
             try
-            {
+            {   
                 if (ModelState.IsValid) ///checks if the pass in eventData is valid
                 {
+                    // 1. Create a new Event instance based on the input event
                     var newEvent = new Event
                     {
                         EventName = eventData.EventName,
@@ -40,13 +41,14 @@ namespace SportMeApp.Controllers.EventCreation
                         PaypalAccount = eventData.PaypalAccount
                     };
                     _logger.LogInformation("LOG: New Event Details - EventName: {EventName}, StartTime: {StartTime}, EndTime: {EndTime}, Fee: {Fee}, LocationId: {LocationId}, SportId: {SportId}, PaypalAccount: {PaypalAccount}",
-    newEvent.EventName, newEvent.StartTime, newEvent.EndTime, newEvent.Fee, newEvent.LocationId, newEvent.SportId, newEvent.PaypalAccount);
-
+                    newEvent.EventName, newEvent.StartTime, newEvent.EndTime, newEvent.Fee, newEvent.LocationId, newEvent.SportId, newEvent.PaypalAccount);
+                    // 2. Add the event to the Event table in database
                     _context.Event.Add(newEvent);
+                    // 3. await db to save changes
                     await _context.SaveChangesAsync();
 
                     _logger.LogInformation("LOG:Event created successfully.");
-
+                    // 4. return ok!!
                     return Ok(newEvent);
                 }
 
